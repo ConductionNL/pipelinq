db06adf21e74cff4b4e114153dfa2ba1cb5b84de
Al Gorithm <hydra-builder@conduction.nl>
2026-04-16 21:35:15 +0000

    browser fix: Fix setInterval parameter in CallbackOverdueJob constructor (#227)

--- lib/BackgroundJob/CallbackOverdueJob.php
+++ lib/BackgroundJob/CallbackOverdueJob.php
@@ -72,8 +72,8 @@ class CallbackOverdueJob extends TimedJob
         private NotificationService $notificationService,
         private LoggerInterface $logger,
     ) {
-        parent::__construct(time: $time);
-        $this->setInterval(interval: self::INTERVAL);
+        parent::__construct($time);
+        $this->setInterval(self::INTERVAL);
     }//end __construct()
 
     /**
